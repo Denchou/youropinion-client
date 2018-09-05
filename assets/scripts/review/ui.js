a003172393a569a82fd4eda9c99aeb3947a3610b
denchou <denchou617@gmail.com>
2018-09-05 16:25:41 -0400

    Updated UI and visibility
    Created more UI/UX improvements with HTML and CSS

--- assets/scripts/review/ui.js
+++ assets/scripts/review/ui.js
@@ -4,17 +4,18 @@ const api = require('./api')
 const allReviewsListing = require('./all-reviews.handlebars')
 const myReviewsListing = require('./my-reviews.handlebars')
 
+// ui for successful index call
 const onAllReviewsSuccess = function (data) {
   $('#message').html('Here is a list of all reviews!')
   $('#review').html('')
   const showReviewsHtml = allReviewsListing({ reviews: data.reviews })
   $('#review').append(showReviewsHtml)
 }
-
+// ui response for unsuccessful index call
 const onAllReviewsFailure = function (data) {
   $('#message').html('We are unable to process your request.')
 }
-
+// ui response for successful create
 const onSubmitReviewSuccess = function (response) {
   const data = response.review
   $('#topic').val('')
@@ -23,11 +24,11 @@ const onSubmitReviewSuccess = function (response) {
   $('#reviewModal').modal('toggle')
   $('#message').html('You have successfully submitted your review!')
 }
-
+// ui response for unsuccessful create
 const onSubmitReviewFailure = function (response) {
   $('#message').html('There was an error in submitting your review.')
 }
-
+// ui response for successful user_id query index call
 const onMyReviewsSuccess = function (data) {
   $('#message').html('Your reviews...')
   $('#review').html('')
@@ -35,11 +36,11 @@ const onMyReviewsSuccess = function (data) {
   $('#review').append(showMyReviewsHtml)
   store.reviews = data.reviews
 }
-
+// ui response for unsuccessful user_id query index call
 const onMyReviewsFailure = function (response) {
   $('#message').html('There was an error in retrieving your reviews.')
 }
-
+// ui response for successful delete
 const onDeleteReviewSuccess = function () {
   $('#message').html('You have successfully removed your opinion from the internet. Good job!')
   $('#review').html('')
